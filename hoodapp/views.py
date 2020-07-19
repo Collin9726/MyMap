60b518d91838b3a260066069c99363a08ac3d583
collin9726 <owino.collin@gmail.com>
2020-07-19 11:13:56 +0300

    display admin details on my-admin-profile page

--- hoodapp/views.py
+++ hoodapp/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.decorators import login_required
 from django.core.exceptions import ObjectDoesNotExist
 from django.contrib.auth.models import User
 from .email import send_signup_email_admin, send_signup_email_resident
-from .models import Admin_Profile
+from .models import Admin_Profile, Neighbourhood
 from .forms import AdminProfileForm, NeighbourhoodForm
 
 # Create your views here.
@@ -45,16 +45,53 @@ def create_hood(request):
         admin_profile = Admin_Profile.objects.get(this_user = current_user)
     except Admin_Profile.DoesNotExist:
         raise Http404()
+
+    my_hood = None
+    try:
+        my_hood = Neighbourhood.objects.get(admin = admin_profile)
+    except Neighbourhood.DoesNotExist:
+        pass
+
+    if my_hood:
+        return redirect(my_admin_profile)
+
     if request.method == 'POST':
         form = NeighbourhoodForm(request.POST)
         if form.is_valid():
             hood = form.save(commit=False)
             hood.admin = admin_profile
             hood.save()
-        return redirect(index)
+        return redirect(my_admin_page)
 
     else:
         form = NeighbourhoodForm()
       
     title = "Create Hood"
     return render(request, 'create-hood.html', {"form": form, "title": title})
+
+
+@login_required(login_url='/accounts/login/')
+def my_admin_profile(request):
+    current_user = request.user
+    try:
+        admin_profile = Admin_Profile.objects.get(this_user = current_user)
+    except Admin_Profile.DoesNotExist:
+        raise Http404()
+
+    try:
+        my_hood = Neighbourhood.objects.get(admin = admin_profile)
+    except Neighbourhood.DoesNotExist:
+        raise Http404()
+    # if request.method == 'POST':
+    #     form = NeighbourhoodForm(request.POST)
+    #     if form.is_valid():
+    #         hood = form.save(commit=False)
+    #         hood.admin = admin_profile
+    #         hood.save()
+    #     return redirect(my_admin_page)
+
+    # else:
+    #     form = NeighbourhoodForm()
+      
+    title = admin_profile.this_user.username
+    return render(request, 'my-admin-profile.html', {"profile": admin_profile, "title": title, "hood": my_hood})
