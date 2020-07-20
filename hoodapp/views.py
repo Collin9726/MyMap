720e6d201e37488a5899fee6e87778ecb9bf83ea
collin9726 <owino.collin@gmail.com>
2020-07-20 04:42:26 +0300

    let user delete their profile

--- hoodapp/views.py
+++ hoodapp/views.py
@@ -11,8 +11,27 @@ from .models import Admin_Profile, Neighbourhood, Resident_Profile, Facility, Bu
 from .forms import AdminProfileForm, NeighbourhoodForm, AddResidentForm, FacilityForm
 
 # Create your views here.
+@login_required(login_url='/accounts/login/')
 def index(request):
-    return render(request, 'index.html')
+    current_user = request.user
+    admin_profile = None
+    try:
+        admin_profile = Admin_Profile.objects.get(this_user = current_user)
+    except Admin_Profile.DoesNotExist:
+        pass
+
+    resident_profile = None
+    try:
+        resident_profile = Resident_Profile.objects.get(this_user = current_user)
+    except Resident_Profile.DoesNotExist:
+        pass
+
+    if admin_profile:
+        return redirect(my_admin_profile)
+    elif resident_profile:
+        return redirect(my_user_profile)
+    else:
+        raise Http404    
 
 @login_required(login_url='/accounts/login/')
 def send_email(request):
@@ -105,7 +124,7 @@ def my_admin_profile(request):
         folium.Marker([hosp_latitude,hosp_longitude],
                     popup=f'<p>{hospital.contact}</p>',
                     tooltip=f'{hospital.facility_name}',
-                    icon=folium.Icon(icon='glyphicon-plus-sign', color='brown')).add_to(m), 
+                    icon=folium.Icon(icon='glyphicon-plus-sign', color='purple')).add_to(m), 
     for post in police_posts:
         post_longitude = post.location[0]
         post_latitude = post.location[1]
@@ -152,7 +171,8 @@ def add_resident(request):
             this_resident =User.objects.create_user(username, email, password)
             resident_profile = Resident_Profile(full_name=name, this_user=this_resident, username=username, hood=my_hood)
             resident_profile.save()
-            my_hood.occupants_count = len(Resident_Profile.objects.filter(hood = my_hood))
+            my_hood.occupants_count = len(Resident_Profile.objects.filter(hood = my_hood))+1
+            my_hood.save()
             send_signup_email_resident(name, username, password,admin_profile.full_name, my_hood.hood_name, email)
             
         return redirect(my_admin_profile)
@@ -239,3 +259,66 @@ def add_facility(request):
       
     title = "Add Facility"
     return render(request, 'add-facility.html', {"form": form, "title": title})
+
+
+
+@login_required(login_url='/accounts/login/')
+def my_user_profile(request):
+    current_user = request.user
+    try:
+        resident_profile = Resident_Profile.objects.get(this_user = current_user)
+    except Resident_Profile.DoesNotExist:
+        raise Http404()
+
+    my_hood = resident_profile.hood
+    longitude = my_hood.location[0]
+    latitude = my_hood.location[1]       
+
+    m = folium.Map(location=[latitude, longitude], zoom_start=17)
+    folium.Marker([latitude,longitude],
+                    popup='<h5>My neighbourhood.</h5>',
+                    tooltip=f'{my_hood.hood_name}',
+                    icon=folium.Icon(icon='glyphicon-home', color='blue')).add_to(m),
+    hospitals = Facility.objects.filter(category='hospital')
+    police_posts = Facility.objects.filter(category='police')
+    for hospital in hospitals:
+        hosp_longitude = hospital.location[0]
+        hosp_latitude = hospital.location[1]
+        folium.Marker([hosp_latitude,hosp_longitude],
+                    popup=f'<p>{hospital.contact}</p>',
+                    tooltip=f'{hospital.facility_name}',
+                    icon=folium.Icon(icon='glyphicon-plus-sign', color='purple')).add_to(m), 
+    for post in police_posts:
+        post_longitude = post.location[0]
+        post_latitude = post.location[1]
+        folium.Marker([post_latitude,post_longitude],
+                    popup=f'<p>{post.contact}</p>',
+                    tooltip=f'{post.facility_name}',
+                    icon=folium.Icon(icon='glyphicon-flag', color='darkgreen')).add_to(m), 
+
+    folium.CircleMarker(
+        location=[latitude, longitude],
+        radius=200,
+        popup=f'{my_hood.hood_name}',
+        color='#428bca',
+        fill=True,
+        fill_color='#428bca'
+    ).add_to(m),
+
+    map_page = m._repr_html_()    
+      
+    title = resident_profile.username
+    return render(request, 'my-user-profile.html', {"profile": resident_profile, "title": title, "hood": my_hood, "map_page":map_page})
+
+
+@login_required(login_url='/accounts/login/')
+def delete_resident_profile(request):
+    current_user = request.user
+    try:
+        resident_profile = Resident_Profile.objects.get(this_user = current_user)
+    except Resident_Profile.DoesNotExist:
+        raise Http404()
+    resident_profile.delete()
+    current_user.delete()
+    
+    return redirect(index)
